31097cee5d99c9c0cc8ecf4d5fc43e6a8dd621df
DenisPil <denis.pilongery@gmail.com>
2021-04-01 18:10:28 +0200

    ajout de 2 constants

--- constants.py
+++ constants.py
@@ -1,3 +1,16 @@
 URL = "https://books.toscrape.com/"
-URL_Cat = "https://books.toscrape.com/catalogue/"
+
+URL_CAT = "https://books.toscrape.com/catalogue/"
+
+BOOK = ['https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html']
+
 BOOK_TEST = 'https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+
+CATEGORY = "https://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html"
+
+FIELDNAMES = [
+                'product_page_url', 'universal_product_code',
+                'title', 'price_including_tax', 'price_excluding_tax',
+                'number_available', 'product_description', 'category',
+                'review_rating', 'image_url'
+             ]
